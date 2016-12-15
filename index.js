8fcaacca36392369681e2cb17c7ada63e52ecf89
Alexandro <arvitaly@list.ru>
2016-12-15 14:47:58 +0700

    fix ts

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-var relay_1 = require("./relay");
-exports.Relay = relay_1.default;
+var Relay_1 = require("./Relay");
+exports.Relay = Relay_1.default;
 var QueryParser_1 = require("./QueryParser");
 exports.QueryParser = QueryParser_1.default;
