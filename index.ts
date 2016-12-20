8b5e7a44c00f64fcea71a0e550d058f87dde9b6b
Alexandro <arvitaly@list.ru>
2016-12-20 10:36:20 +0700

    add exports

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { default as Relay } from "./Relay";
+export { default as Relay, IResolver } from "./Relay";
 export { default as QueryParser } from "./QueryParser";
 export * from "./typings";
