d26aacb77f824e0a4e9aa3d7d4dea6096402a6b6
Alexandro <arvitaly@list.ru>
2017-04-04 20:04:24 +0700

    add export

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var Membra_1 = require("./Membra");
 exports.Membra = Membra_1.default;
 var QueryParser_1 = require("./QueryParser");
 exports.QueryParser = QueryParser_1.default;
+var Generator_1 = require("./Generator");
+exports.Generator = Generator_1.default;
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { default as Membra, IQueryResult, IMembraClient, IResolver } from "./Membra";
 export { default as QueryParser } from "./QueryParser";
+export { default as Generator } from "./Generator";
 export * from "./typings";
